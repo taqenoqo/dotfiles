daee85364cf6c5118f3a92e9c798747c202c5bcb
Takenoko <ttaakkee@gmail.com>
2015-02-10 22:32:21 +0900

    ディレクトリ構成変更

---
+++ setup.zsh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+set -eu
+cd $(dirname $0)
+
+for src in */*; do
+    local tgt="$HOME/.${src:t}"
+    ln -siTn "${src:A}" "$tgt"
+done
+
+git submodule update --init --recursive
+
+chsh -s $(where zsh)
+
