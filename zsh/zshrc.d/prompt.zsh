e93a02c7087080155f0ca1feb8033187beeeda31
Takenoko <ttaakkee@gmail.com>
2015-02-23 12:52:19 +0900

    zsh設定の整形

--- zsh/zshrc.d/prompt.zsh
+++ zsh/zshrc.d/prompt.zsh
@@ -1,58 +1,71 @@
-autoload -Uz is-at-least
-if is-at-least 4.3.10; then
-    autoload -Uz vcs_info
-    autoload -Uz add-zsh-hook
-    zstyle ':vcs_info:*' enable git svn hg
-    zstyle ':vcs_info:*' stagedstr '+'
-    zstyle ':vcs_info:*' unstagedstr '-'
-    zstyle ':vcs_info:*' formats '%s %b %c%u'
-    zstyle ':vcs_info:*' actionformats '%s %b [%a] %c%u'
-    zstyle ':vcs_info:*' branchformat '%b:%r'
-    zstyle ':vcs_info:*' use-simple true
-    zstyle ':vcs_info:*' check-for-changes true
-    function updateMessage() {
-        psvar=()
-        LANG=en_US.UTF-8 vcs_info
-        if [[ -n "$vcs_info_msg_0_" ]]; then
-            psvar[1]="$vcs_info_msg_0_"
-        fi
-    }
-    add-zsh-hook precmd updateMessage
-fi
-case $(hostname -s) in
-    "TakenokoMacintosh" )
-        aaColor="%{[38;5;173m%}"
-        ;;
-    "tak3n0k0" )
-        aaColor="%{[38;5;26m%}"
-        ;;
-    abelia[0-5][0-9] | borage[0-5][0-9] | crocus[0-5][0-9] )
-        aaColor="%{[38;5;163m%}"
-        ;;
-    * )
-        aaColor="%{[38;5;244m%}"
-        echo $(hostname -s)
-        ;;
-esac
-if [[ $UID -eq 0 ]]; then
-    promptColor="%{[38;5;197m%}"
-else
-    promptColor=""
-fi
+() {
+    autoload -Uz is-at-least
 
-function change_prompt() {
-    simplePrompt="%# "
-    cPrompt="
-$aaColor     ∧ ∧
+    if is-at-least 4.3.10; then
+        autoload -Uz vcs_info
+        autoload -Uz add-zsh-hook
+        zstyle ':vcs_info:*' enable git svn hg
+        zstyle ':vcs_info:*' stagedstr '+'
+        zstyle ':vcs_info:*' unstagedstr '-'
+        zstyle ':vcs_info:*' formats '%s %b %c%u'
+        zstyle ':vcs_info:*' actionformats '%s %b [%a] %c%u'
+        zstyle ':vcs_info:*' branchformat '%b:%r'
+        zstyle ':vcs_info:*' use-simple true
+        zstyle ':vcs_info:*' check-for-changes true
+        function updateMessage() {
+            psvar=()
+            LANG=en_US.UTF-8 vcs_info
+            if [[ -n "$vcs_info_msg_0_" ]]; then
+                psvar[1]="$vcs_info_msg_0_"
+            fi
+        }
+        add-zsh-hook precmd updateMessage
+    fi
+
+
+    function change_prompt() {
+        local aa_color
+        case $(hostname -s) in
+            "TakenokoMacintosh" )
+                aa_color="%{[38;5;173m%}"
+                ;;
+            "tak3n0k0" )
+                aa_color="%{[38;5;26m%}"
+                ;;
+            abelia[0-5][0-9] | borage[0-5][0-9] | crocus[0-5][0-9] )
+                aa_color="%{[38;5;163m%}"
+                ;;
+            * )
+                aa_color="%{[38;5;244m%}"
+                echo $(hostname -s)
+                ;;
+        esac
+
+        local prompt_color
+        if [[ $UID -eq 0 ]]; then
+            prompt_color="%{[38;5;197m%}"
+        else
+            prompt_color=""
+        fi
+
+        local simple_prompt
+        simple_prompt="%# "
+
+        local c_prompt
+        c_prompt="$aa_color
+     ∧ ∧
     (*ﾟーﾟ) %m:%~
     /  .|   %1v
 ～（＿＿ﾉ
-%n${WINDOW:+"[$WINDOW]"}$promptColor%# %{[38;5;00m%}"
-    if [ $PS1 = $cPrompt ]; then
-        PS1=$simplePrompt
-    else
-        PS1=$cPrompt
-    fi
+%n${WINDOW:+"[$WINDOW]"}$prompt_color%# %{[38;5;00m%}"
+
+        if [ $PS1 = $c_prompt ]; then
+            PS1=$simple_prompt
+        else
+            PS1=$c_prompt
+        fi
+    }
+
+    change_prompt
 }
-change_prompt
 
--- zsh/zshrc.d/setting.zsh
+++ zsh/zshrc.d/setting.zsh
@@ -1,15 +1,18 @@
-export WORDCHARS='*?_.[]~=&;!#$%^(){}<>' #区切り文字
-setopt list_packed #リストを詰めて表示
-setopt auto_pushd #cdのときにpushdする
-stty stop undef #C-s の無効化
+() {
+    export WORDCHARS='*?_.[]~=&;!#$%^(){}<>' #区切り文字
+    setopt list_packed #リストを詰めて表示
+    setopt auto_pushd #cdのときにpushdする
+    stty stop undef #C-s の無効化
 
-# ビープ音を鳴らさない
-setopt nolistbeep
-setopt nobeep
+    # ビープ音を鳴らさない
+    setopt nolistbeep
+    setopt nobeep
 
-bindkey -e #emacs的キーバインド
+    bindkey -e #emacs的キーバインド
 
-setopt glob #グロブ
-setopt extendedglob #拡張グロブ
+    setopt glob #グロブ
+    setopt extendedglob #拡張グロブ
 
-setopt auto_cd #ディレクトリ名を打つと自動でcd
+    setopt auto_cd #ディレクトリ名を打つと自動でcd
+}
+
