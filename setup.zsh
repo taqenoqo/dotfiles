84ab8e71f4af26e0b824f255dbc75a3546c21a4a
Takenoko <ttaakkee@gmail.com>
2016-08-25 11:21:04 +0900

    ディレクトリを平坦化

--- setup.zsh
+++ setup.zsh
@@ -1,13 +1,18 @@
 #!/bin/zsh
 
 set -eu
+autoload -Uz
+
 cd $(dirname $0)
 
-for src in */*; do
-    local tgt="$HOME/.${src:t}"
-    ln -sbTn "${src:A}" "$tgt"
+for src_file in *; do
+    if [[ ${0:a} != ${src_file:a} ]]; then
+	link_file="$HOME/.${src_file:t}"
+	if [[ -L $link_file ]]; then
+	    ln -snfv "${src_file:A}" "$link_file"
+	else
+	    ln -sniv "${src_file:A}" "$link_file"
+	fi
+    fi
 done
-
-git pull --recurse-submodules
-git submodule update --init --recursive
 
