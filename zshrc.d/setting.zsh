de3c3de4f90997742e142b95af777a6789255845
takenoko <ttaakkee@gmail.com>
2022-11-12 02:27:30 +0900

    zsh の履歴関連設定を追加

--- zshrc.d/setting.zsh
+++ zshrc.d/setting.zsh
@@ -21,6 +21,15 @@ bindkey -v '^n' down-line-or-history
 bindkey -v '^_' run-help
 bindkey -v '^y' history-search-forward
 bindkey -v '^h' history-search-backward
+bindkey -v '^r' history-incremental-search-backward
+bindkey -v '^s' history-incremental-search-forward
+
+# ヒストリー設定
+export HISTFILE=$HOME/.zsh_history
+export HISTSIZE=1000
+export SAVEHIST=100000
+setopt EXTENDED_HISTORY # ヒストリーにコマンドの時刻も記録
+setopt SHARE_HISTORY # 複数タブでヒストリーを共有
 
 setopt glob #グロブ
 setopt extendedglob #拡張グロブ
