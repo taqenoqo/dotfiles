19337f719f5d807a90fbffe19c1cd20032d9fa3d
taqenoqo <ttaakkee@gmail.com>
2023-03-03 17:57:10 +0900

    oh my zsh プラグイン追加

--- zshrc.d/0-oh-my-zsh.zsh
+++ zshrc.d/0-oh-my-zsh.zsh
@@ -70,7 +70,7 @@ HIST_STAMPS="yyyy/mm/dd"
 # Custom plugins may be added to $ZSH_CUSTOM/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git docker docker-compose)
+plugins=(git docker docker-compose aws)
 
 source $ZSH/oh-my-zsh.sh
 
