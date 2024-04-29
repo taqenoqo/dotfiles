6f6253ca33eb67e6a22a76b53d7ca3a8668c088f
taqenoqo <ttaakkee@gmail.com>
2024-04-29 17:58:07 +0900

    ls コマンド修正

--- zshrc.d/alias.zsh
+++ zshrc.d/alias.zsh
@@ -1,7 +1,7 @@
 if ls --color >/dev/null 2>&1; then
-    alias ls='ls -Flh -N --color=auto --time-style +%Y-%m-%d\ %H:%M'
+    alias ls='ls -Flh -N --color=auto --time-style +%Y-%m-%d\ %H:%M --group-directories-first'
 elif ls -G >/dev/null 2>&1; then
-    alias ls='ls -GFlh --group-directories-first'
+    alias ls='ls -GFlh'
 fi
 
 alias cp='cp -i'
